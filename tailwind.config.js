a6c498bbffe2c7afeb4f7d45f543b880f25a0802
satinota <notasati24@gmail.com>
2024-09-06 16:30:46 +0200

    add:structur-project

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const { DEFAULT_CIPHERS } = require('tls');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    fontFamily: {
+		primary: "var(--font-jetbrainMono)",
+	},
+    extend: {
+      colors: {
+        primary: "#151E3F",
+		accent: {
+DEFAULT: "#C16E70",
+hover: "#E6E7C7",
+		},
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
