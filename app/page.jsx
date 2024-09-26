5e32596fe0477406d5e5f4071465f67416b290a8
satinota <notasati24@gmail.com>
2024-09-26 10:37:22 +0200

    add:page-services

--- app/page.jsx
+++ app/page.jsx
@@ -1,29 +1,48 @@
+'use client'; 
+import Photo from "@/components/Photo";
+import Social from "@/components/Social";
+import Stats from "@/components/Stats";
 import {Button} from "@/components/ui/button";
 import {FiDownload} from 'react-icons/fi'
 
 const Home = () => {
+  const handleDownload = () => {
+    const cvUrl = '/assets/Satenik-SAAKIAN-CV.pdf'; 
+    const link = document.createElement('a');
+    link.href = cvUrl;
+    link.download = 'Satenik-SAAKIAN-CV.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Full-stack Developer</span>
             <h1 className="h1">Hi! I am <br/> <span className="text-accent">Sati Nota</span></h1>
             <p className="max-w-[500px] mb-9 text-white/80">Full stack developer with one year of frontend experience and an extensive 8-year background in marketing. Quick to master new skills, I create functional and visually appealing web applications, combining technical expertise with a deep understanding of user needs and business requirements.</p>
           <div className="flex flex-col xl:flex-row items-center gap-8">
-            <Button variant="outline" size="lg" className="uppercase flex items-center  gap-2">
+            <Button 
+              variant="outline" 
+              size="lg" 
+              className="uppercase flex items-center gap-2"
+              onClick={handleDownload} 
+            >
               <span>Download CV</span>
               <FiDownload className="text-xl"/>
             </Button>
-            <div>socials</div>
+            <div className="mb-8 xl:mb-0"><Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transitions-all duration-500"/></div>
           </div>
           </div>
-          <div></div>
+          <div className="mb-8 xl:mb-0 order-1 xl:order-none"><Photo/></div>
         </div>
       </div>
+      <Stats/>
     </section>
   )
 }
 
 export default Home
-
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,9 +1,68 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import { BsArrowDownRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+
+const services = [
+    {
+      num: "01",
+      title: "Web Development",
+      descr:
+        "As a React developer, I specialize in building responsive and interactive user interfaces. I excel in creating reusable components, managing state with hooks, and integrating RESTful APIs to develop efficient and user-friendly web applications.",
+    },
+    {
+      num: "02",
+      title: "Digital Marketing",
+      descr:
+        "I develop and implement effective strategies across various digital platforms. My approach includes social media management, email marketing, and content creation. Using data-driven methods, I optimize campaigns to increase engagement and conversions.",
+    },
+    {
+      num: "03",
+      title: "UI/UX Design",
+      descr:
+        "My focus is on understanding clients and their needs deeply. I create intuitive interfaces based on thorough user experience analysis. This approach allows me to develop solutions that fully meet the expectations of the target audience and enhance user satisfaction.",
+    },
+    {
+      num: "04",
+      title: "Logo Design",
+      descr:
+        "I craft visually striking and memorable logos that capture brand essence. Combining artistic creativity with strategic thinking, I ensure logos are versatile across various media. My designs effectively communicate brand values and enhance overall brand recognition.",
+    },
+  ];
 
 const Services = () => {
-    return (
-        <div className="text-with"> page</div>
-    )
-}
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={0}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.3, ease: "easeIn" },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+            {services.map((service, index) => {
+                return (
+                    <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
+                        <div className="w-full flex justify-between items-center">
+                            <div className="text-5xl font-extrabold">
+                                {service.num}
+                            </div>
+                            <BsArrowDownRight/>
+                        </div>
+                        <h2 className="text-xl">{service.title}</h2>
+                        <p className="text">{service.descr}</p>
+                        <div className="border-b border-white/20 w-full"></div>
 
-export default Services
+                    </div>
+                )
+            })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
