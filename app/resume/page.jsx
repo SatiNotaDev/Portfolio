26a762a873b5d1a2ec5285bc706dd227503de8d4
satinota <notasati24@gmail.com>
2024-09-26 16:18:12 +0200

    add:page-resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,9 +1,212 @@
+"use client";
 import React from "react";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiFigma,
+} from "react-icons/si";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+
+const about = {
+  title: "About me",
+  description:
+    "A professional with expertise in digital marketing and web development. Two years ago, I moved to France where I quickly adapted and continued my professional growth. I'm confident that my technical skills and drive for growth make me a valuable asset to any team working on innovative web projects.",
+  info: [
+    { fieldName: "Name", fielValue: "Sati Saakian" },
+    { fieldName: "Phone", fielValue: "+33 6 02 20 96 62" },
+    { fieldName: "Telegram", fielValue: "sati_saakian" },
+    { fieldName: "Email", fielValue: "notasati24@gmail.com" },
+    { fieldName: "Experience marketing", fielValue: "8+ Years" },
+    { fieldName: "Experience development", fielValue: "1+ Years" },
+    { fieldName: "Languages", fielValue: "French, English, Russian" },
+  ],
+};
+
+const experience = {
+  icon: "s/assets/resume/badge.svg",
+  title: "My experiance",
+  description:
+    "Starting my career in marketing, I developed skills in SEO, SMM, and advertising campaign management. My passion for technology then led me to web development, where I now focus on Full Stack technologies, including React and Node.js. My diverse experience allows me to work effectively at the intersection of technology and business.",
+  item: [
+    {
+      company: "Plan9",
+      position: "Junior front-end developpeur",
+      duration: "mars - aout 2024",
+    },
+    {
+      company: "Médiathèque de Tarentaize",
+      position: "Stagiaire en informatique",
+      duration: "jan - fev 2024",
+    },
+    {
+      company: "MTS télécommunications",
+      position: "Junior front-end developpeur",
+      duration: "mai - oct 2022",
+    },
+    {
+      company: "MTS télécommunications",
+      position: "Responsable marketing digital",
+      duration: "jan 2022 - mai 2022",
+    },
+    {
+      company: "Centre de certification MÉJOREGIONTEST",
+      position: "Responsable marketing digital",
+      duration: "2019-2020",
+    },
+    {
+      company: "China today",
+      position: "Responsable marketing",
+      duration: "2017-2019",
+    },
+  ],
+};
+
+const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My education",
+  description:
+    "Diverse educational background, combining university training in marketing with recent specialization in web development. Continuous commitment to learning and improving technical and language skills.",
+  item: [
+    {
+      institution: "IT-Akademy (fr)",
+      program: "Full-stack developeur",
+      duration: "may 2024 - avril 2025",
+    },
+    {
+      institution: "Projexia (fr)",
+      program: "Cours de français В1-В2",
+      duration: "nov 2023 - mars 2024",
+    },
+    {
+      institution: "Skillbox - formation en ligne (rus)",
+      program: "Front-End Developpeur",
+      duration: "2021 - 2023",
+    },
+    {
+      institution: "Université Économique d'État de Saint-Pétersbourg",
+      program: "Spécialiste en relations publiques et marketing",
+      duration: "2008 - 2013",
+    },
+  ],
+};
+
+const skills = {
+  title: "My skills",
+  description:
+    "Versatile professional with a unique blend of web development and digital marketing expertise. Proficient in full-stack technologies including HTML, CSS, JavaScript, React, and Node.js, complemented by strong skills in SEO, SMM, and data analytics, enabling the creation of comprehensive, user-centric digital solutions.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "html 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "css 3",
+    },
+    {
+      icon: <FaJs />,
+      name: "javascript",
+    },
+    {
+      icon: <FaReact />,
+      name: "react.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "tailwid.css",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "next.js",
+    },
+    {
+      icon: <SiNodedotjs />,
+      name: "node.js",
+    },
+    {
+      icon: <SiFigma />,
+      name: "figma",
+    },
+  ],
+};
 
 const Resume = () => {
-    return (
-        <div>resume</div>
-    )
-}
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+          <div className="min-w-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.item.map((item, index) => {
+                      return (
+                        <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1 ">
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-w-[60px] text-center lg:text-left">{item.position}</h3>
+                          <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              experians
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              experians
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              experians
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
+};
 
-export default Resume
+export default Resume;
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,67 +1,97 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { BsArrowDownRight } from "react-icons/bs";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const services = [
-    {
-      num: "01",
-      title: "Web Development",
-      descr:
-        "As a React developer, I specialize in building responsive and interactive user interfaces. I excel in creating reusable components, managing state with hooks, and integrating RESTful APIs to develop efficient and user-friendly web applications.",
-    },
-    {
-      num: "02",
-      title: "Digital Marketing",
-      descr:
-        "I develop and implement effective strategies across various digital platforms. My approach includes social media management, email marketing, and content creation. Using data-driven methods, I optimize campaigns to increase engagement and conversions.",
-    },
-    {
-      num: "03",
-      title: "UI/UX Design",
-      descr:
-        "My focus is on understanding clients and their needs deeply. I create intuitive interfaces based on thorough user experience analysis. This approach allows me to develop solutions that fully meet the expectations of the target audience and enhance user satisfaction.",
-    },
-    {
-      num: "04",
-      title: "Logo Design",
-      descr:
-        "I craft visually striking and memorable logos that capture brand essence. Combining artistic creativity with strategic thinking, I ensure logos are versatile across various media. My designs effectively communicate brand values and enhance overall brand recognition.",
-    },
-  ];
+  {
+    num: "01",
+    title: "Web Development",
+    descr:
+      "As a React developer, I specialize in building responsive and interactive user interfaces. I excel in creating reusable components, managing state with hooks, and integrating RESTful APIs to develop efficient and user-friendly web applications.",
+    link: "",
+  },
+  {
+    num: "02",
+    title: "Digital Marketing",
+    descr:
+      "I develop and implement effective strategies across various digital platforms. My approach includes social media management, email marketing, and content creation. Using data-driven methods, I optimize campaigns to increase engagement and conversions.",
+    link: "",
+  },
+  {
+    num: "03",
+    title: "UI/UX Design",
+    descr:
+      "My focus is on understanding clients and their needs deeply. I create intuitive interfaces based on thorough user experience analysis. This approach allows me to develop solutions that fully meet the expectations of the target audience and enhance user satisfaction.",
+    link: "",
+  },
+  {
+    num: "04",
+    title: "Logo Design",
+    descr:
+      "I craft visually striking and memorable logos that capture brand essence. Combining artistic creativity with strategic thinking, I ensure logos are versatile across various media. My designs effectively communicate brand values and enhance overall brand recognition.",
+    link: "",
+  },
+];
 
 const Services = () => {
-  return (
-    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-      <div className="container mx-auto">
-        <motion.div
-          initial={0}
-          animate={{
-            opacity: 1,
-            transition: { delay: 2.4, duration: 0.3, ease: "easeIn" },
-          }}
-          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
-        >
-            {services.map((service, index) => {
-                return (
-                    <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
-                        <div className="w-full flex justify-between items-center">
-                            <div className="text-5xl font-extrabold">
-                                {service.num}
-                            </div>
-                            <BsArrowDownRight/>
-                        </div>
-                        <h2 className="text-xl">{service.title}</h2>
-                        <p className="text">{service.descr}</p>
-                        <div className="border-b border-white/20 w-full"></div>
+  const [isVisible, setIsVisible] = useState(false);
 
+  useEffect(() => {
+    // Задержка видимости контента
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 1400); // Настройте эту задержку, чтобы она соответствовала длительности анимации перехода страницы
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <AnimatePresence>
+      {isVisible && (
+        <motion.section
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0"
+        >
+          <div className="container mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-[60px]">
+              {services.map((service, index) => {
+                return (
+                  <motion.div
+                    key={index}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: index * 0.1, ease: "easeOut"}}
+                    className="flex-1 flex flex-col justify-center gap-6 group"
+                  >
+                    <div className="w-full flex justify-between items-center">
+                      <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                        {service.num}
+                      </div>
+                      <Link
+                        href={service.link}
+                        className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                      >
+                        <BsArrowDownRight className="text-primary text-3xl" />
+                      </Link>
                     </div>
-                )
-            })}
-        </motion.div>
-      </div>
-    </section>
+                    <h2 className="text-3xl font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                      {service.title}
+                    </h2>
+                    <p className="text-white/60">{service.descr}</p>
+                    <div className="border-b border-white/20 w-full"></div>
+                  </motion.div>
+                );
+              })}
+            </div>
+          </div>
+        </motion.section>
+      )}
+    </AnimatePresence>
   );
 };
 
